6da545e1dd0884634874f493dced2985e7528371
Dieguin <diego.segura@geniac.com>
2017-03-30 18:24:28 +0200

    :tada: [components] add carousel component

---
+++ components/Carousel.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="carousel">
+  <transition name="bounce" appear>
+    <div>
+      <carousel arrows="true" dots="true">
+        <slide slot="slides" :style="{ 'background-color': '#f6f6f6' }">
+          <div>
+            <h1>We are awesome</h1>
+            <span>Fucking, really, awesome</span>
+          </div>
+        </slide>
+        <slide slot="slides" :style="{ 'background-color': '#ffc107' }">
+          <svg>
+          <use xlink:href="#search"></use>
+        </svg>
+          <div>
+            <h1>Search just because!</h1>
+            <span>Yes, this is a search icon i got in my side <b>just for fun</b></span>
+          </div>
+        </slide>
+        <slide slot="slides" :style="{ 'background-color': '#03a9f4' }">
+          <div>
+            <svg>
+            <use xlink:href="#computer"></use>
+          </svg>
+          </div>
+          <div>
+            <h1>Hosting</h1>
+            <span>Optimizamos el despliegue de tu aplicación, porque la velocidad y la seguridad importan.</span>
+          </div>
+        </slide>
+      </carousel>
+    </div>
+
+  </transition>
+</div>
+</template>
+
+<script>
+const Carousel = () =>
+  import ('~/components/carousel/Carousel');
+const Slide = () =>
+  import ('~/components/carousel/Slide');
+import Navigation from '~/components/Navigation';
+
+export default {
+  components: {
+    Carousel,
+    Slide,
+    Navigation,
+  }
+}
+</script>
